3e7c04c0eb425a3f1f03dd46a26e50dfa9918d20
alexcerium <alexmatkava@gmail.com>
2025-04-17 16:39:36 +0300

    Initial Commit

---
+++ CoffeDaily/CoffeDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeDailyApp.swift
+//  CoffeDaily
+//
+//  Created by Aleksandr on 17.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoffeDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeDailyTests/CoffeDailyTests.swift
@@ -0,0 +1,16 @@
+//
+//  CoffeDailyTests.swift
+//  CoffeDailyTests
+//
+//  Created by Aleksandr on 17.04.2025.
+//
+
+import Testing
+
+struct CoffeDailyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
